395483fd8b6277c5525f45406bbae68c909d107e
vuongpm <phanvuong118@gmail.com>
2026-02-22 22:34:28 +0700

    first commit

---
+++ src/main/java/com/happycat/meetingappbe/MeetingAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.happycat.meetingappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/happycat/meetingappbe/MeetingAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happycat.meetingappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
