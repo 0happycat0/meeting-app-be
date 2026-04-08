407423e31090398d55ca9e3411934f454820abab
vuongpm <phanvuong118@gmail.com>
2026-04-08 16:31:38 +0700

    Create ApiResponse to wrap response

---
+++ src/main/java/com/happycat/meetingappbe/dto/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.happycat.meetingappbe.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    @Builder.Default // Phải đánh dấu field này là default
+    int code = 1000;
+    String message;
+    T result;
+}
