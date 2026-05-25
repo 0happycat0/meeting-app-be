32b15b404e6ef0873139c5defd576e47a4fc3ba3
vuongpm <phanvuong118@gmail.com>
2026-05-25 10:23:06 +0700

    Update ApiResponse to use Integer for code and add PageResponse class for paginated responses

--- src/main/java/com/happycat/meetingappbe/dto/PageResponse.java
+++ src/main/java/com/happycat/meetingappbe/dto/PageResponse.java
@@ -4,15 +4,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ApiResponse<T> {
-    @Builder.Default // Phải đánh dấu field này là default
-    Integer code = 1000;
-    String message;
-    T result;
+public class PageResponse<T> {
+    List<T> items;
+    Long total;
 }
