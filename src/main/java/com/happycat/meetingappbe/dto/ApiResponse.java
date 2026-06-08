2f5ef80b823f4ba73930fe3f87d7400adc815eac
vuongpm <phanvuong118@gmail.com>
2026-06-09 01:48:58 +0700

    Add LiveKit configuration and update application settings

--- src/main/java/com/happycat/meetingappbe/dto/ApiResponse.java
+++ src/main/java/com/happycat/meetingappbe/dto/ApiResponse.java
@@ -11,7 +11,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
-    @Builder.Default // Phải đánh dấu field này là default
+    @Builder.Default
     Integer code = 1000;
     String message;
     T result;
